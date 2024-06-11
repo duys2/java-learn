ce6866930ae5643e7c8e34b1b7fe5b5acb3fb54d
Ays <minigun5@naver.com>
2024-06-12 02:33:43 +0900

    [Add] 함수 기초

---
+++ src/FunctionLearn.java
@@ -0,0 +1,15 @@
+public class FunctionLearn {
+	public static void main(String[] args) {
+		Test test = new Test();
+
+		int num = test.add(3, 4, 5);
+
+		System.out.println(num); // 12
+	}
+}
+
+class Test {
+	public int add(int a, int b, int c) {
+		return a + b + c;
+	}
+}
