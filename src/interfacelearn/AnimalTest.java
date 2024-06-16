f5fa86638266e2208731c7f5074bb06664230564
Ays <minigun5@naver.com>
2024-06-17 00:36:18 +0900

    [Add] 인터페이스

---
+++ src/interfacelearn/AnimalTest.java
@@ -0,0 +1,15 @@
+package interfacelearn;
+
+public class AnimalTest {
+	public static void main(String[] args) {
+		Tiger tiger = new Tiger();
+		Wolf wolf = new Wolf();
+
+		tiger.eat(); // 호랑이가 먹이를 먹는 중입니다.
+		wolf.eat(); // 늑대가 먹이를 먹는 중입니다.
+		tiger.sleep(); // 호랑이가 자는 중입니다.
+		wolf.sleep(); // 늑대가 수면 중입니다.
+		tiger.walk(); // 호랑이가 걷는 중입니다.
+		wolf.walk(); // 늑대가 걷는 중입니다.
+	}
+}
---
+++ src/interfacelearn/Tiger.java
@@ -0,0 +1,19 @@
+package interfacelearn;
+
+// Tiger 클래스는 Animal 인터페이스를 구현한다.
+public class Tiger implements Animal {
+	@Override
+	public void eat() {
+		System.out.println("호랑이가 먹이를 먹는 중입니다.");
+	}
+
+	@Override
+	public void sleep() {
+		System.out.println("호랑이가 자는 중입니다.");
+	}
+
+	@Override
+	public void walk() {
+		System.out.println("호랑이가 걷는 중입니다.");
+	}
+}
