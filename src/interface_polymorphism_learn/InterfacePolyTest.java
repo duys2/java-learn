c8e18db22f35e757c5a91db7a34c0cf7fc9e7add
Ays <minigun5@naver.com>
2024-06-18 08:14:01 +0900

    [Add] 인터페이스 다형성
    오버로딩한 메서드 사용
    : 매개변수로 구현 객체를 전달해서 사용하기

--- src/interface_polymorphism_learn/InterfacePolyTest.java
+++ src/interface_polymorphism_learn/InterfacePolyTest.java
@@ -3,12 +3,18 @@ package interface_polymorphism_learn;
 public class InterfacePolyTest {
 	public static void main(String[] args) {
 		ProfileService profileService = new ProfileService();
-		ProfileDBRepository profileDBRepository = new ProfileDBRepository();
-		ProfileMemoryRepository profileMemoryRepository = new ProfileMemoryRepository();
 
 		profileService.saveProfile(); // 출력: 메모리에 프로필 저장하기
 		profileService.repository.save(); // 출력: 메모리에 프로필 저장하기
+
+		/* 각 구현 객체로 출력해 보기 */
+		ProfileDBRepository profileDBRepository = new ProfileDBRepository();
+		ProfileMemoryRepository profileMemoryRepository = new ProfileMemoryRepository();
+
 		profileDBRepository.save(); // 출력: DB에 프로필 저장하기
 		profileMemoryRepository.save(); // 출력: 메모리에 프로필 저장하기
+
+		/* 오버로드한 메서드 호출: 매개변수로 구현 객체 전달 == profileDBRepository.save() 실행 */
+		profileService.saveProfile(profileDBRepository); // 출력: DB에 프로필 저장하기
 	}
 }
