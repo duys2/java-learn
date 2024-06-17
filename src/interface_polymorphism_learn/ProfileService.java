55b0d039abd573df3d3e6c0f193c6ea770b90169
Ays <minigun5@naver.com>
2024-06-18 08:16:35 +0900

    [Add] 인터페이스 다형성: 오버로드
    누락된 파일 커밋 및 푸시

--- src/interface_polymorphism_learn/ProfileService.java
+++ src/interface_polymorphism_learn/ProfileService.java
@@ -1,13 +1,18 @@
 package interface_polymorphism_learn;
 
 public class ProfileService {
-	ProfileRepository repository; // 인터페이스는 매개변수에서도, 필드에서도 선언 가능
+	ProfileRepository repository; // 인터페이스는 필드에서도 선언 가능
 
 	public void saveProfile() {
 		/* 이 부분만 바꾸면 된다: 구현 객체를 교체하면서 프로그램의 실행 결과가 다양해지는 것이 바로 인터페이스의 다형성 */
 		repository = new ProfileMemoryRepository();
 		// repository = new ProfileDBRepository();
 
+		repository.save();
+	}
+
+	// 오버로딩: 인터페이스는 매개변수에서도 선언 가능
+	public void saveProfile(ProfileRepository repository) {
 		repository.save();
 	}
 }
