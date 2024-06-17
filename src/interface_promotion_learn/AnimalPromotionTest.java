79c021ab785997b1cd524ef0c825d235c89a5388
Ays <minigun5@naver.com>
2024-06-18 08:43:05 +0900

    [Comment] 자동 타입 변환 주석 수정

--- src/interface_promotion_learn/AnimalPromotionTest.java
+++ src/interface_promotion_learn/AnimalPromotionTest.java
@@ -2,7 +2,7 @@ package interface_promotion_learn;
 
 public class AnimalPromotionTest {
 	public static void main(String[] args) {
-		/* 자동타입변환 */
+		/* 자동 타입 변환 */
 		Carnivore carnivore = new Carnivore();
 
 		Tiger tiger = new Tiger();
@@ -12,10 +12,10 @@ public class AnimalPromotionTest {
 		carnivore.eat(tiger); // 호랑이가 먹이를 먹는 중입니다.
 		carnivore.eat(wolf); // 늑대가 먹이를 먹는 중입니다.
 
-		// Animal을 Tiger로 자동타입변환
+		// Animal을 Tiger로 자동 타입 변환
 		Animal firstAnimal = new Tiger();
 
-		// Animal을 Wolf로 자동타입변환
+		// Animal을 Wolf로 자동 타입 변환
 		Animal secondAnimal = new Wolf();
 
 		carnivore.eat(firstAnimal); // 호랑이가 먹이를 먹는 중입니다.
